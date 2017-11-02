f11bf18aaa4dedf8a806c7336506a303fb2b1a6b
vubui237 <vtb0977@gmail.com>
2017-11-01 21:57:30 -0500

    Webpack Bundle

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
 const ce = React.createElement;
 
 const MyTitle = function(props) {
@@ -15,4 +19,4 @@ const MyFirstComponent = function() {
   );
 };
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById('app'));
